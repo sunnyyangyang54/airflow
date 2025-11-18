93adf36f00faf41858b35885ed7316945cbb6b31
yang <yangyang54@gmail.com>
2025-11-18 12:52:00 +0900

    bash_operator dags 업로드

---
+++ dags/dags_bash_operator.py
@@ -0,0 +1,22 @@
+from airflow.sdk import DAG
+import datetime
+import pendulum
+from airflow.providers.standard.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_operator",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2025, 11, 18, tz="UTC"),
+    catchup=False,
+) as dag:
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="echo whoami",
+    )    
+
+    bash_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_command="echo $HOSTNAME",
+    )    
+    
+    bash_t1 >> bash_t2

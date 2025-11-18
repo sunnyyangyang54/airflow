e4d9787255310f246f68e4cee72c6fa108bcdcb9
yang <yangyang54@gmail.com>
2025-11-18 13:48:26 +0900

    aa

--- dags/dags_bash_operator2.py
+++ dags/dags_bash_operator2.py
@@ -4,19 +4,19 @@ import pendulum
 from airflow.providers.standard.operators.bash import BashOperator
 
 with DAG(
-    dag_id="dags_bash_operator",
+    dag_id="dags_bash_operator2",
     schedule="0 0 * * *",
     start_date=pendulum.datetime(2025, 11, 18, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
     bash_t1 = BashOperator(
         task_id="bash_t1",
-        bash_command="echo test1",
+        bash_command="echo aa",
     )    
 
     bash_t2 = BashOperator(
         task_id="bash_t2",
-        bash_command="echo test2",
+        bash_command="echo bb",
     )    
     
     bash_t1 >> bash_t2

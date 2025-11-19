1946ab481fe99667f1a53f5cf90edcca598906fb
yang <yangyang54@gmail.com>
2025-11-19 14:47:17 +0900

    arg test

---
+++ dags/dags_python_with_op_args.py
@@ -0,0 +1,18 @@
+from airflow.sdk import DAG
+import pendulum
+import datetime
+from airflow.providers.standard.operators.python import PythonOperator
+from common.commom_func import regist
+
+with DAG(
+    dag_id="dags_python_with_op_args",
+    schedule="0 2 * * 1",
+    start_date=pendulum.datetime(2025, 11, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+
+    regist_t1 = PythonOperator(
+        task_id='regist_t1',
+        python_callable=regist,
+        op_args=['yang','man','kr','seoul']
+    )
--- plugins/common/commom_func.py
+++ plugins/common/commom_func.py
@@ -1,5 +1,9 @@
 def get_sftp():
     print("get_sftp test")
 
-def get_data():
-    print("함수를 실행합니다.")
+    def regist(name,sex,*args):
+        print(f"이름은 {name}이고 성별은 {sex}입니다.")
+        print(name)
+        print(sex)
+        print("기타옵현들: {args}")
+        

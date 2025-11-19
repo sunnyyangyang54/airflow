c70bd379d4f2a563f31592437457195852eb4672
yang <yangyang54@gmail.com>
2025-11-19 14:55:53 +0900

    func 수정

--- plugins/common/commom_func.py
+++ plugins/common/commom_func.py
@@ -1,9 +1,8 @@
 def get_sftp():
     print("get_sftp test")
 
-    def regist(name,sex,*args):
-        print(f"이름은 {name}이고 성별은 {sex}입니다.")
-        print(name)
-        print(sex)
-        print("기타옵현들: {args}")
-        
+def regist(name,sex,*args):
+    print(f"이름은 {name}이고 성별은 {sex}입니다.")
+    print(name)
+    print(sex)
+    print("기타옵현들: {args}")
